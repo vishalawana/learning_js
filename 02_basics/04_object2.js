3b9640188ecc9ccdb2b2a2688a9462adf9f7f909
vishal awana <53907738+vishalawana@users.noreply.github.com>
2023-10-03 15:12:14 +0000

    object in js

--- 02_basics/04_object2.js
+++ 02_basics/04_object2.js
@@ -27,7 +27,7 @@ const obj2 = { 3: "a",4:"b"};
 // const obj3 = Object.assign({},obj1,obj2);//{}behave like a source
 // console.log(obj3);
 const obj3 = {...obj1,...obj2};// concatination using spread operator
-console.log(obj3); 
+// console.log(obj3); 
 //another way to play with objects in js when data comes from a database it comes as array ob objects
 const users = [
     {
@@ -51,10 +51,32 @@ const users = [
         email:"hello.com"
     }
 ]
-console.log(users[1].email);
-// some other usefull methods of js objects
-console.log(tinderUser)
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(users[1].email);
+ // some other usefull methods of js objects
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+const course = { 
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"vishal"
+}
+course.courseInstructor;// it is a overhead process to write this type of long line of code instead ae can do "object destructuring"
+
+const {courseInstructor: instructor} = course;
+//now we can use 
+console.log(instructor);// will be evaluate same as course.courseInstructor;
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// } this is json and this is what we get from an api
+[
+    {},
+    {},
+    {}
+]// sometimes we can get this type of response of api
